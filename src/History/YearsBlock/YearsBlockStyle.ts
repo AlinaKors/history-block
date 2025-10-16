cd7b5451f554a7408e994b56f6217c86b7e6c50a
Alina Korsukova <korsualin@gmail.com>
2025-10-16 20:21:44 +0300

    fix fonts

--- src/History/YearsBlock/YearsBlockStyle.ts
+++ src/History/YearsBlock/YearsBlockStyle.ts
@@ -4,13 +4,17 @@ export const Year = styled.div<{ color: string }>`
   color: ${(props) => props.color};
   font-size: 10rem;
   font-weight: bold;
+  line-height: 160px;
   @media (max-width: 889px) {
     font-size: 7.8rem;
+    line-height: 120px;
   }
   @media (max-width: 767px) {
     font-size: 5.7rem;
+    line-height: 88px;
   }
   @media (max-width: 599px) {
     font-size: 4rem;
+    line-height: 56px;
   }
 `;
